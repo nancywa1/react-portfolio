1bd6f924af07f8fe76eeea98599d1cf0bd6d893a
nancywa1 <nancy.wang0717@gmail.com>
2022-06-19 19:49:50 -0600

    try to fix blank deploy UI

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import resume from "../../assets/Resume.pdf";
+import resume from "../../assets/file/Resume.pdf";
 function Resume(props) {
   return (
     <section className="my-5">
